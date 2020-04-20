ea3551f58f3702c52fd98836117986f3c809ed9f
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-04-20 22:09:17 +0800

    update

--- src/minesweeper.java
+++ src/minesweeper.java
@@ -1,18 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
 class Game extends JFrame {
-    private int width=800,height=650;
-    Game(){
+    private int width = 800, height = 650, row = 10, col = 50;
+
+    Game() {
         setSize(width, height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setVisible(true);
         setTitle("Minesweeper");
+        setLocationRelativeTo(this);
     }
 }
+
 public class minesweeper {
-    public static void main(String[] args){
-        Game g=new Game();
+    public static void main(String[] args) {
+        Game g = new Game();
     }
 }
