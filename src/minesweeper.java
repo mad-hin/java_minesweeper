bd5176143b8f7e860c2010195c732389f0138f77
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-04-20 19:05:27 +0800

    update minesweeper.java

--- src/minesweeper.java
+++ src/minesweeper.java
@@ -3,6 +3,7 @@ public class minesweeper {
     public static void main(String[] args) {
         JFrame m = new JFrame();
         m.setSize(800,600);
+        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         m.setVisible(true);
     }
 }
