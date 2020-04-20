9ae7e8de88e5b33274fc5b20ddddad62eec5c62d
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-04-20 19:02:14 +0800

    update

--- src/minesweeper.java
+++ src/minesweeper.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
 public class minesweeper {
-
+    public static void main(String[] args) {
+        JFrame m = new JFrame();
+        m.setSize(800,600);
+        m.setVisible(true);
+    }
 }
