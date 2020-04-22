94f8b09c7621013d98572886d7b977fa45362b4d
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-04-22 22:03:31 +0800

    update minesweeper.java

--- src/minesweeper.java
+++ src/minesweeper.java
@@ -1,23 +1,24 @@
 import javax.swing.*;
-import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Dialog;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.MenuItem;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-class Game extends JFrame implements ActionListener {
+class Game extends JFrame implements ActionListener{//, MouseListener {
     private int width = 800, height = 650, row = 10, col = 10;
     private int mines = 99;
+    private boolean[][] isTurned;
+    private boolean gameOver;
+    private boolean gameRunning;
+    private int[][] aroundBombNum;
+    private boolean[][] map;
+    private boolean[][] isPressed;
+    private JButton[][] buttons;
+    private JPanel minePanel;
+    private JLabel gameMessage;
 
     Game() {
         setSize(width, height);
@@ -36,14 +37,22 @@ class Game extends JFrame implements ActionListener {
         restart.addActionListener(this);
         file.add(restart);
         MenuItem end = new MenuItem("End Game");
+        end.setActionCommand("endGame");
+        end.addActionListener(this);
         file.add(end);
         menu.add(file);
 
         //second menu : "Level"
         Menu level = new Menu("Level");
         MenuItem beginner = new MenuItem("Beginner");
+        beginner.setActionCommand("beg");
+        beginner.addActionListener(this);
         MenuItem intermediate = new MenuItem("Intermediate");
+        intermediate.setActionCommand("mid");
+        intermediate.addActionListener(this);
         MenuItem expert = new MenuItem("Expert");
+        expert.setActionCommand("exp");
+        expert.addActionListener(this);
         MenuItem custom = new MenuItem("Custom");
         level.add(beginner);
         level.add(intermediate);
@@ -53,8 +62,24 @@ class Game extends JFrame implements ActionListener {
 
         //interface
         JPanel minePanel = new JPanel();
-        add(minePanel);
+        minePanel.setLayout(new GridLayout(col,row));
+        //minePanel.addMouseListener(this);
 
+        //buttons (a.k.a mines)
+        buttons= new JButton[col][row];
+        for(int i = 0; i < col; i++){
+            for (int j = 0; j < row;j++){
+                buttons[i][j] = new JButton();
+                buttons[i][j].setBackground(Color.WHITE);
+                buttons[i][j].setActionCommand(i+" "+j);
+                minePanel.add(buttons[i][j]);
+                //buttons[i][j].addMouseListener(this);
+                buttons[i][j].addActionListener(this);
+            }
+        }
+        add(minePanel, BorderLayout.CENTER);
+
+        //make it visible
         setVisible(true);
     }
 
@@ -66,6 +91,23 @@ class Game extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e){
         String command = e.getActionCommand();
     }
+
+    /*@Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }*/
 }
 
 public class minesweeper {
